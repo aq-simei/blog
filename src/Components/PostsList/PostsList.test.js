688592d46e2947c3716e28c9a672b63d876c4527
Antonio Simei <antonio.simei@codeminer42.com>
2022-04-04 17:24:07 -0300

    Update test message

--- src/Components/PostsList/PostsList.test.js
+++ src/Components/PostsList/PostsList.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { PostsList } from "./index";
 
 describe("PostList", () => {
-  it("Renders a list of posts", () => {
+  it("Renders a Posts List Header", () => {
     render(<PostsList />);
 
     const postsListHeader = screen.getByText("Posts List");
