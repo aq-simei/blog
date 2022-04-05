53cb59bf598179fd64894699c4c825014e85d2f4
Antonio Simei <antonio.simei@codeminer42.com>
2022-04-05 18:59:18 -0300

    Refactor test(WIP)

--- src/Components/PostsList/PostsList.test.js
+++ src/Components/PostsList/PostsList.test.js
@@ -1,13 +1,25 @@
-import { render, screen } from "../../Utils/test-utils";
+import { render, screen, waitFor } from "../../Utils/test-utils";
 import { PostsList } from "./index";
 import { mockedPosts } from "./Posts_mocks";
 
 describe("PostsList", () => {
-  it("Renders the first post title ", () => {
-    render(<PostsList />, { postsContextValue: { posts: mockedPosts } });
+  describe("When there are no posts yet", () => {
+    it("Renders the loading message ", () => {
+      render(<PostsList />, { postsContextValue: { posts: mockedPosts } });
+      const loadingMessage = screen.getByText(/loading\.\.\./i);
+      expect(loadingMessage).toBeInTheDocument();
+    });
+  });
+  describe("When there are available posts to be rendered", () => {
+    it("Renders the posts", async () => {
+      render(<PostsList />, { postsContextValue: { posts: mockedPosts } });
 
-    const firstPostTitle = screen.getByText("Test title 1");
+      await waitFor(() => {
+        screen.getByText(/Test title 1/i);
+      });
+      const testPost = screen.getByText(/Test title /i);
 
-    expect(firstPostTitle).toBeInTheDocument();
+      expect(testPost).toBeInTheDocument();
+    });
   });
 });
