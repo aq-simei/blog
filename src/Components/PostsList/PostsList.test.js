ae1b2774979a615246a8ceefcc065fd30090e657
Antonio Simei <antonio.simei@codeminer42.com>
2022-04-07 13:57:28 -0300

    Improve PostsList test with more accurate  expectations

--- src/Components/PostsList/PostsList.test.js
+++ src/Components/PostsList/PostsList.test.js
@@ -9,15 +9,32 @@ describe("PostsList", () => {
         postsContextValue: { posts: mockedPosts, isLoading: true },
       });
       const loadingMessage = screen.getByText(/loading\.\.\./i);
+      
       expect(loadingMessage).toBeInTheDocument();
     });
   });
   describe("When data is fetched", () => {
-    it("Renders the posts", async () => {
+    it("Renders a list of 3 posts", async () => {
       render(<PostsList />, {
         postsContextValue: { posts: mockedPosts, isLoading: false },
       });
-      expect(mockedPosts).toHaveLength(3);
+      const firstTestPostTitle = screen.getByText(/Test title 1/i);
+      const firstTestPostIdentifier = screen.getByText(/#1/i);
+
+      const secondTestPostTitle = screen.getByText(/Test title 2/i);
+      const secondTestPostIdentifier = screen.getByText(/#2/i);
+
+      const thirdTestPostTitle = screen.getByText(/Test title 3/i);
+      const thirdTestPostIdentifier = screen.getByText(/#3/i);
+
+      expect(firstTestPostTitle).toBeInTheDocument();
+      expect(firstTestPostIdentifier).toBeInTheDocument();
+
+      expect(secondTestPostTitle).toBeInTheDocument();
+      expect(secondTestPostIdentifier).toBeInTheDocument();
+
+      expect(thirdTestPostTitle).toBeInTheDocument();
+      expect(thirdTestPostIdentifier).toBeInTheDocument();
     });
   });
 });
