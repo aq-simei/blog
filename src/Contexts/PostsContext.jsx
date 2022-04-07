379b309eb9bdcc31d2b4e03a2b6ec4e6fa606d9a
Antonio Simei <antonio.simei@codeminer42.com>
2022-04-07 13:44:17 -0300

    Fix nested API requests

--- src/Contexts/PostsContext.jsx
+++ src/Contexts/PostsContext.jsx
@@ -19,57 +19,21 @@ export const PostsProvider = ({ children }) => {
 
   useEffect(() => {
     const getPosts = () => {
-      api
-        .get("/posts?_start=0&_limit=20")
-        .then((res) => {
-          setPosts(res.data);
-        })
-        .catch((err) => {
-          console.log("Failed to load 0~20 posts", err);
-        })
-        .then(
-          api
-            .get("/posts?_start=20&_limit=20")
-            .then((res) => {
+      api.get("/posts?_start=0&_limit=20").then((res) => {
+        setPosts(res.data);
+        api.get("/posts?_start=20&_limit=20").then((res) => {
+            setPosts((posts) => [...posts, ...res.data]);
+            api.get("/posts?_start=40&_limit=20").then((res) => {
               setPosts((posts) => [...posts, ...res.data]);
-            })
-            .catch((err) => {
-              console.log("Failed to load 20~40 posts", err);
-            })
-        )
-        .then(
-          api
-            .get("/posts?_start=40&_limit=20")
-            .then((res) => {
-              setPosts((posts) => [...posts, ...res.data]);
-            })
-            .catch((err) => {
-              console.log("Failed to load posts 40~60", err);
-            })
-        )
-        .then(
-          api
-            .get("/posts?_start=60&_limit=20")
-            .then((res) => {
-              setPosts((posts) => [...posts, ...res.data]);
-            })
-            .catch((err) => {
-              console.log("Failed to load posts 60~80", err);
-            })
-        )
-        .then(
-          api
-            .get("/posts?_start=80&_limit=15")
-            .then((res) => {
-              setPosts((posts) => [...posts, ...res.data]);
-            })
-            .catch((err) => {
-              console.log("Failed to load posts 80~95", err);
-            })
-        )
-        .finally(() => {
-          setIsLoading(false);
-        });
+              api.get("/posts?_start=60&_limit=20").then((res) => {
+                setPosts((posts) => [...posts, ...res.data]);
+                api.get("/posts?_start=80&_limit=15").then((res) => {
+                  setPosts((posts) => [...posts, ...res.data]);
+                }).finally(() => {setIsLoading(false)});
+              }).catch((err) => {console.log(err.toJSON())});;
+            }).catch((err) => {console.log(err.toJSON())});;
+          }).catch((err) => {console.log(err.toJSON())});
+      }).catch((err) => {console.log(err.toJSON())});
     };
     getPosts();
   }, []);
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
