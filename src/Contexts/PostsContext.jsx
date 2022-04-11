2284eb1b842d637040da62dd9f41d0faa07a4a0f
Antonio Simei <antonio.simei@codeminer42.com>
2022-04-11 06:39:34 -0300

    Refactor: implement listPosts request outside the context function

--- src/Contexts/PostsContext.jsx
+++ src/Contexts/PostsContext.jsx
@@ -1,9 +1,9 @@
 import { createContext, useEffect } from "react";
 import { useContext, useState } from "react";
+import { api } from "../services/api.js";
 
-import {listPosts} from '../services/post.js';
+import { listPosts } from "../services/post.js";
 import { listPostCommentaries } from "../services/commentary";
-
 
 export const PostsContext = createContext();
 
@@ -14,57 +14,70 @@ export const PostsProvider = ({ children }) => {
   useEffect(() => {
     const firstRequestParams = {
       _start: 0,
-      _limit: 20
-  }
+      _limit: 20,
+    };
+    const secondRequestParams = {
+      _start: 20,
+      _limit: 20,
+    };
+    const thirdRequestParams = {
+      _start: 40,
+      _limit: 20,
+    };
+    const fourthRequestParams = {
+      _start: 60,
+      _limit: 20,
+    };
+    const fifthRequestParams = {
+      _start: 80,
+      _limit: 15,
+    };
     const getPosts = () => {
       return listPosts(firstRequestParams)
         .then((res) => {
           Promise.all(listPostCommentaries(res.data)).then(
             (postWithCommentaries) => {
               setPosts(postWithCommentaries);
-              setIsLoading(false);
             }
           );
+          listPosts(secondRequestParams).then((res) => {
+            Promise.all(listPostCommentaries(res.data)).then(
+              (postWithCommentaries) => {
+                setPosts((posts) => [...posts, ...postWithCommentaries]);
+              }
+            );
+            listPosts(thirdRequestParams).then((res) => {
+              Promise.all(listPostCommentaries(res.data)).then(
+                (postWithCommentaries) => {
+                  setPosts((posts) => [...posts, ...postWithCommentaries]);
+                }
+              );
 
-          // api
-          //   .get("/posts?_start=20&_limit=20")
-          //   .then((res) => {
-          //     setPosts((posts) => [...posts, ...res.data]);
+              listPosts(fourthRequestParams).then((res) => {
+                Promise.all(listPostCommentaries(res.data)).then(
+                  (postWithCommentaries) => {
+                    setPosts((posts) => [...posts, ...postWithCommentaries]);
+                  }
+                );
 
-          //     api
-          //       .get("/posts?_start=40&_limit=20")
-          //       .then((res) => {
-          //         setPosts((posts) => [...posts, ...res.data]);
-
-          //         api
-          //           .get("/posts?_start=60&_limit=20")
-          //           .then((res) => {
-          //             setPosts((posts) => [...posts, ...res.data]);
-
-          //             api
-          //               .get("/posts?_start=80&_limit=15")
-          //               .then((res) => {
-          //                 setPosts((posts) => [...posts, ...res.data]);
-          //               })
-          //               .finally(() => {
-          //                 setIsLoading(false);
-          //               });
-          //           })
-          //           .catch((err) => {
-          //             console.log(err.toJSON());
-          //           });
-          //       })
-          //       .catch((err) => {
-          //         console.log(err.toJSON());
-          //       });
-          //   })
-          //   .catch((err) => {
-          //     console.log(err.toJSON());
-          //   });
+                listPosts(fifthRequestParams).then((res) => {
+                  Promise.all(listPostCommentaries(res.data)).then(
+                    (postWithCommentaries) => {
+                      setPosts((posts) => [...posts, ...postWithCommentaries]);
+                    }
+                  );
+                })
+                .catch((err) => {
+                  console.log(err.toJSON());
+              })
+              }).finally(() => {setIsLoading(false)});
+              
+            });
+          });
         })
-        // .catch((err) => {
-        //   console.log(err.toJSON());
-        // });
+        .catch((err) => {
+          console.log(err.toJSON());
+        });
     };
     getPosts();
   }, []);
